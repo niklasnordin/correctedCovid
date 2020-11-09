9da07ea63b4da26e70c18d6f604dbe9010806d4f
Niklas Nordin <niklas.nordin@nequam.se>
2020-11-09 20:48:38 +0100

    added plot function for owid data

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ https://covid19.who.int/table
 
 weekly number of tests
 https://www.statista.com/statistics/1133055/weekly-number-of-coronavirus-tests-in-sweden/
+
+to plot this data run:
+python plt_curve.py
+
+to plot data from
+https://github.com/owid/covid-19-data/blob/master/public/data/owid-covid-codebook.csv
+
+python plt_owid.py
---
+++ plt_owid.py
@@ -0,0 +1,71 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import datetime
+import matplotlib.dates as mdates
+
+import pandas as pd
+import numpy as np
+from scipy.optimize import minimize
+
+filename="owid-covid-data.csv"
+data = pd.read_csv(filename, sep=",")
+
+days=7
+days2=14
+country="SWE"
+c = data["iso_code"]
+
+nc="new_cases"
+nd="new_deaths"
+da="date"
+pr="positive_rate"
+nt='new_tests'
+
+cases=[]
+deaths=[]
+date=[]
+pos_rate=[]
+normalized_cases=[]
+tests=[]
+
+for i,d in enumerate(c):
+    if d==country:
+        cases.append(data[nc][i])
+        deaths.append(data[nd][i])
+        date.append(data[da][i])
+        pos_rate.append(data[pr][i])
+        tests.append(data[nt][i])
+
+def_test=100000.0/7.0
+for i,d in enumerate(pos_rate):
+    normalized_cases.append(def_test*d)
+
+
+free_test_date = [ "2020-06-04" ]
+free_test_case = [ 4000.0 ]
+
+fig, ax = plt.subplots(figsize=(15,6))
+
+dco=np.convolve(cases, np.ones(days)/days, mode='same')
+dco2=np.convolve(cases, np.ones(days2)/days2, mode='same')
+dcn=np.convolve(normalized_cases, np.ones(days)/days, mode='same')
+dcn2=np.convolve(normalized_cases, np.ones(days2)/days2, mode='same')
+
+plt.plot(date,dco, color='blue')
+plt.plot(date,dcn, color='red')
+
+plt.bar(date,cases, width=1, alpha=0.5, color='blue', label="cases")
+plt.bar(date,normalized_cases, width=0.5, alpha=0.5, color='red', label="Normalized for 100 000 tests")
+plt.bar(free_test_date,free_test_case, width=1, alpha=0.5, color="green", label="Start of free testing")
+
+xt = ax.get_xticks()
+ax.set_xticks(np.arange(xt[0],xt[np.size(xt)-1], 30))
+plt.legend()
+plt.grid()
+plt.xlabel("Date")
+plt.ylabel("number of positive tests")
+plt.title("Number of positive corona tests in Sweden")
+plt.savefig("cases.png")
+plt.show()
+
+#print(tests)
